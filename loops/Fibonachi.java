768adff1db630adbf8676c207aa60e875d97dc2f
mentorvikash <mentorvikash@gmail.com>
2024-02-24 02:11:39 +0530

    find number of times repete

--- loops/Fibonachi.java
+++ loops/Fibonachi.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Fibonachi {
     public static void main(String[] args) {
-        // get fibonachi number at n postion
+
+        // ========== get fibonachi number at n postion
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+        // int n = input.nextInt();
+        int n = 15;
 
         // first two number
         int a = 0;
@@ -21,5 +23,19 @@ public class Fibonachi {
         }
 
         System.out.println("fibonachi number at " + n + " is: " + b);
+
+        // ========= find how many times a number repetes.
+        int testNumber = 1212122;
+        int count = 0;
+
+        while (testNumber > 0) {
+            int reminder = testNumber % 10;
+            // System.out.println(reminder is );
+            if (reminder == 2) {
+                count++;
+            }
+            testNumber = testNumber / 10;
+        }
+        System.out.println("the number 2 repeate times " + count);
     }
 }
