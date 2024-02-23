5bd3ca5307b0a1ed76e54248d52acea767d32ec1
mentorvikash <mentorvikash@gmail.com>
2024-02-24 01:32:46 +0530

    find max of three

--- loops/Condition1.java
+++ loops/Condition1.java
@@ -1,5 +1,7 @@
 package loops;
 
+import java.util.Scanner;
+
 public class Condition1 {
 
     public static void main(String[] args) {
@@ -8,26 +10,26 @@ public class Condition1 {
 
         int n = 97;
 
-        if (n < 100) {
-            System.out.println("you are not elegible for party town");
-        } else {
-            System.out.println("you are elegible for party town");
-        }
+        // if (n < 100) {
+        // System.out.println("you are not elegible for party town");
+        // } else {
+        // System.out.println("you are elegible for party town");
+        // }
 
         // multiple if else statement - Salary hike
 
-        int salary = 35251;
-        int finalSalary = 0;
+        // int salary = 35251;
+        // int finalSalary = 0;
 
-        if (salary <= 10000) {
-            finalSalary = salary + 1000;
-        } else if (salary > 10000 && salary <= 50000) {
-            finalSalary = salary + 3000;
-        } else if (salary > 50000 && salary <= 100000) {
-            finalSalary = salary + 5000;
-        } else if (salary > 100000) {
-            finalSalary = salary + 10000;
-        }
+        // if (salary <= 10000) {
+        // finalSalary = salary + 1000;
+        // } else if (salary > 10000 && salary <= 50000) {
+        // finalSalary = salary + 3000;
+        // } else if (salary > 50000 && salary <= 100000) {
+        // finalSalary = salary + 5000;
+        // } else if (salary > 100000) {
+        // finalSalary = salary + 10000;
+        // }
 
         // System.out.println(" your final salary is " + finalSalary);
 
@@ -58,12 +60,26 @@ public class Condition1 {
 
         // do while loop
 
-        do {
-            System.out.println(i);
-            i++;
-        } while (i <= num);
+        // do {
+        // System.out.println(i);
+        // i++;
+        // } while (i <= num);
 
         // find the larget of three input given by user
+        Scanner input = new Scanner(System.in);
 
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        int number3 = input.nextInt();
+
+        int max = number1;
+        if (number2 > max) {
+            max = number2;
+        }
+        if (number3 > max) {
+            max = number3;
+        }
+
+        System.out.println("maximum of all are " + max);
     }
 }
